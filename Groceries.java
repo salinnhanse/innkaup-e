f2c8a809867ff23947eddbf2b20733a703f013b6
stefaj9 <stefaj9@gmail.com>
2016-10-06 09:02:42 +0000

    Added the Groceries class.

---
+++ Groceries.java
@@ -0,0 +1,10 @@
+class Groceries {
+    private String[] items;
+    public Groceries() {
+    	items = new String[20];
+    }
+
+    public static void main(String args[]) {
+	System.out.println("Hello World!");
+    }
+}
